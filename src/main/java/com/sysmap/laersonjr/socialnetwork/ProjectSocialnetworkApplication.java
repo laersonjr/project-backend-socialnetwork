0b1fcd4fae11c890ba214483010c87b0a1a364e6
laersonjr <laersonjr@gmail.com>
2023-04-30 12:36:12 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/sysmap/laersonjr/socialnetwork/ProjectSocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.sysmap.laersonjr.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSocialnetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSocialnetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sysmap/laersonjr/socialnetwork/ProjectSocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmap.laersonjr.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSocialnetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
