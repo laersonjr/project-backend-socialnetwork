5dec81011a81e65d54fdebf82389eaee60eec0db
laersonjr <laersonjr@gmail.com>
2023-05-01 10:01:09 -0300

    :sparkles:feat (modelDTO) Criado UsuarioRequestBody

---
+++ src/main/java/com/sysmap/laersonjr/socialnetwork/api/modelDTO/input/UsuarioRequestBody.java
@@ -0,0 +1,28 @@
+package com.sysmap.laersonjr.socialnetwork.api.modelDTO.input;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class UsuarioRequestBody {
+
+    private UUID id;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String senha;
+
+    @NotBlank
+    private String apelido;
+
+    private String urlFoto;
+
+}
