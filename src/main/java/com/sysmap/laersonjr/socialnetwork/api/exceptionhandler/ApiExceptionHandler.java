842030372ff2ac3ccb2dbde0a404ee6974137b4e
laersonjr <laersonjr@gmail.com>
2023-05-01 15:57:37 -0300

    :sparkles:feat Criando ControllerAdvice para tratamento de exceções

---
+++ src/main/java/com/sysmap/laersonjr/socialnetwork/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.sysmap.laersonjr.socialnetwork.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        List<ErrorDetails.ProblemError> errors = getProblemErrors(ex.getBindingResult().getAllErrors());
+
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setStatus(status.value());
+        errorDetails.setTitle(messageSource.getMessage("invalid-fields", null, LocaleContextHolder.getLocale()));
+        errorDetails.setProblemErrorList(errors);
+
+        return handleExceptionInternal(ex, errorDetails, headers, status, request);
+    }
+
+    private List<ErrorDetails.ProblemError> getProblemErrors(List<ObjectError> allErrors) {
+        return allErrors.stream()
+                .map(objectError -> new ErrorDetails.ProblemError(((FieldError) objectError).getField(),
+                        messageSource.getMessage(objectError, LocaleContextHolder.getLocale())))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/com/sysmap/laersonjr/socialnetwork/api/exceptionhandler/ErrorDetails.java
@@ -0,0 +1,26 @@
+package com.sysmap.laersonjr.socialnetwork.api.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorDetails {
+
+    private Integer status;
+    private String title;
+    private List<ProblemError> problemErrorList;
+
+    @AllArgsConstructor
+    @Getter
+    public static class ProblemError{
+
+        private String field;
+        private String message;
+    }
+
+}
